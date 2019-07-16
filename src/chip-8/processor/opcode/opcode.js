e2a14383d460776198975ba82d3775264c480dac
Maxim Kalinovskiy <malekylik@gmail.com>
2019-07-16 22:00:53 +0300

    processor/feat: Add mock game

--- src/chip-8/processor/opcode/opcode.js
+++ src/chip-8/processor/opcode/opcode.js
@@ -3,12 +3,16 @@ import { LEAST_SIGNIFICANT_BYTES, MOST_SIGNIFICANT_BYTES } from './const';
 export function createOpcode(bytes) {
   return {
     uint8: new Uint8Array(bytes),
-    uint16: new Uint16Array(bytes),
+    uint16: new Uint16Array([bytes[0] | (bytes[1] << 8)]),
   }
 }
 
 export function getPrefixValue(opcode) {
   return opcode.uint8[MOST_SIGNIFICANT_BYTES] & 0xF;
+}
+
+export function getValueWithourPrefix(opcode) {
+  return opcode.uint16[0] & 0x0FFF;
 }
 
 export function getLeastByte(opcode) {
--- src/index.js
+++ src/index.js
@@ -2,9 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './main.css';
+import { creatProcessor, executeOpcode } from './chip-8/processor/processor';
+import { MOCK_GAME } from './chip-8/processor/const';
+import { createOpcode } from './chip-8/processor/opcode/opcode';
 
 class ABC extends React.Component {
-    state = { a: 'asd' }
+  state = { a: 'asd' }
 }
 
 ReactDOM.render(<span className='span'>hello</span>, document.getElementById('app'));
+
+const processor = creatProcessor();
+
+for (let i = 0; i + 2 < MOCK_GAME.length; i += 2) {
+  executeOpcode(processor, createOpcode(MOCK_GAME.subarray(i, i + 2)));
+}

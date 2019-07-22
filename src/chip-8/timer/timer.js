e111d2b8eeecb371e7af015cb28fbe7c4932ba17
Maxim Kalinovskiy <malekylik@gmail.com>
2019-07-22 20:55:15 +0300

    timer/feat: Add timer

---
+++ src/chip-8/timer/timer.js
@@ -0,0 +1,27 @@
+export function creatTimer() {
+  const bytes = new ArrayBuffer(1);
+
+  return {
+    timerValue: new Uint8Array(bytes),
+  };
+}
+
+export function getTimerValue(timer) {
+  return timer.timerValue[0];
+}
+
+export function setTimerValue(timer, value) {
+  return timer.timerValue[0] = value;
+}
+
+export function decrementTimer(timer) {
+  return setTimerValue(timer, getTimerValue(timer) - 1);
+}
+
+export function updateTimer(timer) {
+  if (getTimerValue(timer) > 0) {
+    return decrementTimer(timer) === 0;
+  }
+
+  return false;
+}

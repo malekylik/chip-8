51fa021fbd7761e03659de48ab5b8d86f9233c61
Maxim Kalinovskiy <malekylik@gmail.com>
2019-07-27 21:01:17 +0300

    canvas/feat: Add width and height props to Canvas

--- src/components/without-jsx/canvas/canvas.js
+++ src/components/without-jsx/canvas/canvas.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export default class Canvas extends React.Component {
   render() {
+    const { width, height } = this.props;
+
     return (
-      React.createElement('canvas', null, null)
+      React.createElement('canvas', { width, height }, null)
     );
   }
+}
+
+Canvas.propTypes = {
+  width: PropTypes.number,
+  height: PropTypes.number,
+};
+
+Canvas.defaultProps = {
+  width: 300,
+  height: 150,
 }

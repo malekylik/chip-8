fca6a1825c98820191a5a63d5ee70f73562325c7
Maxim Kalinovskiy <malekylik@gmail.com>
2019-07-27 20:53:50 +0300

    canvas/feat: Add Canvas component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Canvas from './components/without-jsx/canvas/canvas';
+
 import { MOCK_GAME, TEST_ROM } from './chip-8/processor/const';
 import { executeNextCycly, getDisplay } from './chip-8/chip-8';
 import { getPixel } from './chip-8/display/display';
@@ -13,11 +15,11 @@ import { getAssemblerForOpcode } from './chip-8/debugger/debugger';
 
 import './main.css';
 
-ReactDOM.render(<span className='span'>hello</span>, document.getElementById('app'));
+ReactDOM.render(React.createElement(Canvas, null, null), document.getElementById('app'));
 
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
-const canvasBuffer = ctx.getImageData(0, 0, canvas.width, canvas.height);
+// const canvas = document.getElementById('canvas');
+// const ctx = canvas.getContext('2d');
+// const canvasBuffer = ctx.getImageData(0, 0, canvas.width, canvas.height);
 
 function putPixel(buffer, x, y, value) {
   const color = value * 255;
@@ -42,13 +44,13 @@ function main() {
 
   const display = getDisplay(chip8);
 
-  for (let i = 0; i < DISPLAY_HEIGHT * scale; i++) {
-    for (let j = 0; j < DISPLAY_WIDTH * scale; j++) {
-      putPixel(canvasBuffer, j, i, getPixel(display, (j / scale) | 0, (i / scale) | 0));
-    }
-  }
+  // for (let i = 0; i < DISPLAY_HEIGHT * scale; i++) {
+  //   for (let j = 0; j < DISPLAY_WIDTH * scale; j++) {
+  //     putPixel(canvasBuffer, j, i, getPixel(display, (j / scale) | 0, (i / scale) | 0));
+  //   }
+  // }
 
-  ctx.putImageData(canvasBuffer, 0, 0);
+  // ctx.putImageData(canvasBuffer, 0, 0);
 
   console.log(`${PC} - ${getAssemblerForOpcode(opcode)}`);
 }

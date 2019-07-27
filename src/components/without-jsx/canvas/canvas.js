25f1219092a6851ea861fe52f32ddf27a13efad0
Maxim Kalinovskiy <malekylik@gmail.com>
2019-07-27 21:58:23 +0300

    canvas/feat: Add method to set ImageData to Canvas

--- src/components/without-jsx/canvas/canvas.js
+++ src/components/without-jsx/canvas/canvas.js
@@ -2,11 +2,29 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default class Canvas extends React.Component {
+  canvasRef = React.createRef();
+  ctx = null;
+
+  componentDidMount() {
+    this.ctx = this.canvasRef.current.getContext('2d');
+    this.setImageData(this.props.imageData);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setImageData(nextProps.imageData);
+  }
+
+  setImageData(imageData) {
+    if (imageData) {
+      this.ctx.putImageData(imageData, 0, 0);
+    }
+  }
+
   render() {
     const { width, height } = this.props;
 
     return (
-      React.createElement('canvas', { width, height }, null)
+      React.createElement('canvas', { width, height, ref: this.canvasRef }, null)
     );
   }
 }
@@ -14,6 +32,7 @@ export default class Canvas extends React.Component {
 Canvas.propTypes = {
   width: PropTypes.number,
   height: PropTypes.number,
+  imageData: PropTypes.object,
 };
 
 Canvas.defaultProps = {
--- src/index.js
+++ src/index.js
@@ -1,58 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Canvas from './components/without-jsx/canvas/canvas';
-
-import { MOCK_GAME, TEST_ROM } from './chip-8/processor/const';
-import { executeNextCycly, getDisplay } from './chip-8/chip-8';
-import { getPixel } from './chip-8/display/display';
-import { DISPLAY_WIDTH, DISPLAY_HEIGHT } from './chip-8/display/const/index';
-import { createChip8 } from './chip-8/chip-8';
-import { readOpcode } from './chip-8/memory/memory';
-import { createOpcode } from './chip-8/processor/opcode/opcode';
-import { getProgramCounter } from './chip-8/processor/methods';
-import { getAssemblerForOpcode } from './chip-8/debugger/debugger';
+import App from './components/without-jsx/app/app';
 
 import './main.css';
 
-ReactDOM.render(React.createElement(Canvas, null, null), document.getElementById('app'));
-
-// const canvas = document.getElementById('canvas');
-// const ctx = canvas.getContext('2d');
-// const canvasBuffer = ctx.getImageData(0, 0, canvas.width, canvas.height);
-
-function putPixel(buffer, x, y, value) {
-  const color = value * 255;
-
-  let offset = 4 * x + buffer.width * 4 * y;
-  buffer.data[offset++] = color;
-  buffer.data[offset++] = color;
-  buffer.data[offset++] = color;
-  buffer.data[offset++] = 255;
-}
-
-const chip8 = createChip8(MOCK_GAME);
-// const chip8 = createChip8(TEST_ROM);
-
-const scale = 5;
-
-function main() {
-  requestAnimationFrame(main);
-
-  const PC = executeNextCycly(chip8);
-  const opcode = createOpcode(readOpcode(chip8.memory, getProgramCounter(chip8.processor)));
-
-  const display = getDisplay(chip8);
-
-  // for (let i = 0; i < DISPLAY_HEIGHT * scale; i++) {
-  //   for (let j = 0; j < DISPLAY_WIDTH * scale; j++) {
-  //     putPixel(canvasBuffer, j, i, getPixel(display, (j / scale) | 0, (i / scale) | 0));
-  //   }
-  // }
-
-  // ctx.putImageData(canvasBuffer, 0, 0);
-
-  console.log(`${PC} - ${getAssemblerForOpcode(opcode)}`);
-}
-
-main();
+ReactDOM.render(React.createElement(App, null, null), document.getElementById('app'));

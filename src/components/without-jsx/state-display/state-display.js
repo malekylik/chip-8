37aca34c493bf0b1fb4efe833918f06a220d69e9
Maxim Kalinovskiy <malekylik@gmail.com>
2019-07-29 21:41:43 +0300

    state-display/feat: Add PC, RegisterI, DelayTimer and SoundTimer components

--- src/components/without-jsx/state-display/state-display.js
+++ src/components/without-jsx/state-display/state-display.js
@@ -2,20 +2,31 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import Register from './register/register';
+import RegisterI from './register-i/register-i';
+import DelayTimer from './delay-timer/delay-timer';
+import SoundTimer from './sound-timer/sound-timer';
+import ProgramCounter from './program-counter/program-counter';
 
 import './state-display.css';
 
 export default class StateDisplay extends React.Component {
   render() {
-    const registers = this.props. registers.map(
+    const { registerI, delayTimer, soundTimer, programCounter } = this.props;
+    const registers = this.props.registers.map(
       (value, i) => React.createElement(Register, { value, number: i, key: i })
     );
 
     return (
-      React.createElement('div', null,
-        React.createElement('div', { className: 'registers' },
+      React.createElement('div', { className: 'state-display' },
+        React.createElement('div', { className: 'state-display__registers', key: 'registers' },
           registers
-        )
+        ),
+        React.createElement('div', { className: 'state-display__chip-8-state', key: 'chip-8-state' },
+          React.createElement(RegisterI, { value: registerI }),
+          React.createElement(DelayTimer, { value: delayTimer }),
+          React.createElement(SoundTimer, { value: soundTimer }),
+          React.createElement(ProgramCounter, { value: programCounter }),
+        ),
       )
     );
   }
@@ -23,4 +34,8 @@ export default class StateDisplay extends React.Component {
 
 StateDisplay.propTypes = {
   registers: PropTypes.arrayOf(PropTypes.number).isRequired,
+  registerI: PropTypes.number.isRequired,
+  delayTimer: PropTypes.number.isRequired,
+  soundTimer: PropTypes.number.isRequired,
+  programCounter: PropTypes.number.isRequired,
 };

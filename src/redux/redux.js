09d80f72cf72e0ff5d15324f5eed34e814f5f466
Maxim Kalinovskiy <malekylik@gmail.com>
2019-08-18 20:03:39 +0300

    redux/feat: Create shader reducer

--- src/redux/redux.js
+++ src/redux/redux.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 
 import assemblyReducer from './assembly/assembly.reducer';
 import chip8Reducer from './chip-8/chip-8.reducer';
+import shaderReducer from './shader/shader.reducer';
 
 const appReducer = combineReducers({
   assemblyReducer,
   chip8Reducer,
+  shaderReducer,
 });
 
 export default appReducer;
---
+++ src/redux/shader/shader.reducer.js
@@ -0,0 +1,13 @@
+export default function shaderReducer(state = initialState, action) {
+  switch (action.type) {
+  }
+
+  return state;
+}
+
+const initialState = {
+  loaded: false,
+  loading: false,
+  vertSource: '',
+  fragSource: '',
+};
